60db6c5c24e306a1bffa0ad8bc2846ef86a4cd53
Neal Shyam <neal@audioshocker.com>
2014-03-03 19:48:54 -0500

    set a default referral code
    RW5pQ

--- ext/lf.js
+++ ext/lf.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
- 	console.log("set src url: " + localStorage["id"]);
-	document.getElementById("pframe").src = ("http://poncho.is/s/"+localStorage["id"]);
+  console.log("set src url: " + localStorage["id"]);
+ 	
+	if (localStorage["id"] && localStorage["id"] != ""){
+    document.getElementById("pframe").src = ("http://poncho.is/s/"+localStorage["id"]);
+  } else { 
+  		 document.getElementById("pframe").src = ("http://poncho.is/s/RW5pQ");
+  	}
 });
